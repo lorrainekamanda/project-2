8d8329075851f5ca4f42d90188ce64ca9c5ea1db
lorrainekamanda <lorrainekamanda@gmail.com>
2020-02-14 22:46:20 +0300

    add the mathematical variables

---
+++ script.js
@@ -0,0 +1,13 @@
+function akanNames(){
+    var year =document.getElementById("year").value;
+    var MM = parseInt(document.getElementById("month").value);
+    var DD = parseInt(document.getElementById("date").value);
+    var CC = parseInt( year.slice(0,2));
+    var YY = parseInt(year.slice(2,4));
+    
+    
+    
+};
+
+
+
